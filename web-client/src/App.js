92545a01ada7ed2cf7f7d5dedef53a4555c4e4be
Anthony Kesich <akesich+github@gmail.com>
2020-05-18 19:20:14 -0500

    initial web-client commit

---
+++ web-client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Hue to You Too!</h1>
+      </header>
+      <Button>Get Started</Button>
+    </div>
+  );
+}
+
+export default App;
